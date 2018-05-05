7ab0c5f7e008631ea0c091096a75fd90a5445256
vuducdung <hocsinh8195@gmail.com>
2018-05-05 22:12:20 +0700

    dung5-5

--- Public/controller/getData/jsonSchedule.php
+++ Public/controller/getData/jsonSchedule.php
@@ -1,12 +1,12 @@
 <?php
 
- require_once('../../Private/initialize.php');
+ require_once('../../../Private/initialize.php');
 
 header('Content-Type: application/json; charset=UTF-8');
-if(isset($_GET['name'])){
-    $movie_id = $_GET['name'];
+if(isset($_GET['movie_id'])){
+    $movie_id = $_GET['movie_id'];
 
-    $data=Schedule::find_schedule_by_movie($movie_id);
+    $data=Schedule::find_schedule_by_movie(5);
     echo json_encode($data);
 
 
